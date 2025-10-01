2ab6856d83f80c63fb862476328c5d444ffb8a63
devyanshnegi <devyanshnegi@gmail.com>
2025-09-30 19:19:57 -0700

    Updated examples

--- form/urls.py
+++ form/urls.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome Home! You are logged in.")
 
+#urls
 urlpatterns = [
     path("", home, name="home"),  # basic homepage
     path("register/", views.register_new_user, name="register"),
     path("login/", views.login_existing_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
-]
+] 
