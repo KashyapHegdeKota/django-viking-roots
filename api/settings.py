9aba4e2dd05e00344ef3db3fb71356b7b9966d70
Harsh V <102267802+hvmax@users.noreply.github.com>
2025-11-12 15:36:07 -0700

    Add new CORS origin for gimlisaga.org

--- api/settings.py
+++ api/settings.py
@@ -72,6 +72,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://gimlisaga.org"
 ]
 
 # Allow credentials (cookies, sessions)
