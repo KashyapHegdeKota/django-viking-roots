29fe6b3ea21695749d197d76b13073629436b082
Kashyap Hegde Kota <114525863+KashyapHegdeKota@users.noreply.github.com>
2025-11-12 18:37:21 -0700

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -86,6 +86,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://gimlisaga.org"
     # Add your Render backend URL here (e.g., https://your-app.onrender.com)
 ]
 
