bbb23fea7053f6f7b9e4c8f737b974ae66637e05
devyanshnegi <devyanshnegi@gmail.com>
2026-03-30 20:54:42 -0700

    Minor fix

--- community/admin.py
+++ community/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import (
-    FamilyConnection, AncestorMatch, MergedFamilyTree,
+    FamilyConnection, AncestorMatch,
     Post, PostLike, Comment, Group, GroupMembership, GroupPost,
 )
 
 admin.site.register(FamilyConnection)
 admin.site.register(AncestorMatch)
-admin.site.register(MergedFamilyTree)
 admin.site.register(Post)
 admin.site.register(PostLike)
 admin.site.register(Comment)
