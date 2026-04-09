188cc1d32803acd0cbed27ad66cbfc138baedfe1
KashyapHegdeKota <kota2022kashyap@gmail.com>
2026-04-09 01:24:01 -0700

    fix: fixed missing heritage table on render.

--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Fix broken faked migrations
+python fix_heritage_tables.py
+
 python manage.py migrate
